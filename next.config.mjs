71251d598d7f0dbae729e94c39dfdee7dd873999
sun supachai <supachai.sa@kkumail.com>
2025-10-19 21:56:20 +0700

    correct next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // domains: ['localhost'], // ให้ next/image โหลดรูปจาก localhost
+    domains: ['localhost'], // ให้ next/image โหลดรูปจาก localhost
     remotePatterns: [
       {
         // protocol: "http",
