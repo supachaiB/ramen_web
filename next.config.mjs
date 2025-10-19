5acf4d7210795f98f0633be91832869c553f1096
sun supachai <supachai.sa@kkumail.com>
2025-10-19 20:17:17 +0700

    correct next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'], // ให้ next/image โหลดรูปจาก localhost
+    // domains: ['localhost'], // ให้ next/image โหลดรูปจาก localhost
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
+        // protocol: "http",
+        // hostname: "localhost",
+        // port: "3000",
+        // pathname: "/uploads/**",
+
+        protocol: "https",
+        hostname: "ramen-web-b77i.vercel.app",
         pathname: "/uploads/**",
       },
     ],
